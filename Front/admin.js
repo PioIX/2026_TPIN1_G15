636bf81cabae13202d9654d01157fe6c2f19bb3a
Bzona10 <bzona@pioix.edu.ar>
2026-06-29 10:05:28 -0300

    sigo la pagina

---
+++ Front/admin.js
@@ -0,0 +1,27 @@
+function agregarJugador() {
+    window.location.href = "agregarJugador.html";
+}
+
+function editarJugador() {
+    window.location.href = "editarJugador.html";
+}
+
+function eliminarJugador() {
+    window.location.href = "eliminarJugador.html";
+}
+
+function agregarUsuario() {
+    window.location.href = "agregarUsuario.html";
+}
+
+function editarUsuario() {
+    window.location.href = "editarUsuario.html";
+}
+
+function eliminarUsuario() {
+    window.location.href = "eliminarUsuario.html";
+}
+
+function cerrarSesion() {
+    window.location.href = "login.html";
+}
